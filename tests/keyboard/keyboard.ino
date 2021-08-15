0fe46e3fa4bccbfc7c1b7cf5c1aa6dc4489041da
piif <christian_lefebvre@laposte.net>
2021-08-15 23:28:11 +0200

    optimize pin number and algorithm

--- tests/keyboard/keyboard.ino
+++ tests/keyboard/keyboard.ino
@@ -1,34 +1,43 @@
 #include <Arduino.h>
 #include "serialInput.h"
 
-#define K_OUT_CLOCK  8  // White
-#define K_OUT_CLEAR  9  // blue
-#define K_OUT_DATA  10  // gray
+#define K_OUT_CLOCK  8  // blue
+//#define K_OUT_CLEAR  9  // white
+//#define K_OUT_DATA  10  // gray
 
 #define K_IN_C 5    // orange
 #define K_IN_B 6    // yellow
 #define K_IN_A 7    // green
 #define K_IN_G 3    // brown
-#define K_IN_DATA 4 // red
+#define K_DATA 4    // red
 
 void sendBuffer(byte value) {
-	for(byte mask = 0x80; mask; mask >>= 1) {
-		digitalWrite(K_OUT_CLOCK, LOW);
-//		Serial.println((mask & value) ? HIGH : LOW);
-		digitalWrite(K_OUT_DATA, (mask & value) ? HIGH : LOW);
-		digitalWrite(K_OUT_CLOCK, HIGH);
-	}
+	pinMode(K_DATA , OUTPUT);
+//	for(byte mask = 0x80; mask; mask >>= 1) {
+//		digitalWrite(K_OUT_CLOCK, LOW);
+////		Serial.println((mask & value));
+//		digitalWrite(K_DATA, mask & value);
+//		digitalWrite(K_OUT_CLOCK, HIGH);
+//	}
+//	digitalWrite(K_OUT_CLOCK, LOW);
 	digitalWrite(K_OUT_CLOCK, LOW);
+	shiftOut(K_DATA, K_OUT_CLOCK, MSBFIRST, value);
+}
+
+byte readInput() {
+	pinMode(K_DATA , INPUT_PULLUP);
+	digitalWrite(K_IN_G, LOW);
+	byte result = digitalRead(K_DATA);
+	// must disable output just after, to avoid to send data value to shift register (has data pins are shared)
+	digitalWrite(K_IN_G, HIGH);
+	return result;
 }
 
 byte readInput(byte address) {
-	digitalWrite(K_IN_A, (address & 1) ? HIGH : LOW);
-	digitalWrite(K_IN_B, (address & 2) ? HIGH : LOW);
-	digitalWrite(K_IN_C, (address & 4) ? HIGH : LOW);
-	digitalWrite(K_IN_G, LOW);
-	byte result = digitalRead(K_IN_DATA);
-	digitalWrite(K_IN_G, HIGH);
-	return result;
+	digitalWrite(K_IN_A, address & 1);
+	digitalWrite(K_IN_B, address & 2);
+	digitalWrite(K_IN_C, address & 4);
+	return readInput();
 }
 
 char *mapping[8][8] = {
@@ -42,18 +51,72 @@ char *mapping[8][8] = {
 	{	"^",	"cr",	"an",	"v",	"sh",	"<",	">",	"Rt"	}
 };
 
-byte scanFirst() {
-	for(byte mask = 0x80 , line=7; mask; mask >>= 1, line--) {
-		sendBuffer(~mask);
-		for(byte adr = 0; adr < 8; adr++) {
-			if (readInput(adr) == 0) {
-//				Serial.print(line);Serial.print(adr);
-				return (line << 4) | adr;
-//				return mapping[line][7 - adr];
-			}
+byte scanLine() {
+	for(byte adr = 0; adr < 8; adr++) {
+		if (readInput(adr) == 0) {
+			return adr;
 		}
 	}
 	return 0xFF;
+}
+
+byte findLine() {
+	sendBuffer(~0xF0);
+	if (readInput()) {
+		// 0,1,2 or 3
+		sendBuffer(~0xC);
+		if (readInput()) {
+			// 0 or 1
+			sendBuffer(~0x2);
+			if (readInput()) {
+				return 0;
+			} else {
+				return 1;
+			}
+		} else {
+			// 2 or 3
+			sendBuffer(~0x8);
+			if (readInput()) {
+				return 2;
+			} else {
+				return 3;
+			}
+		}
+	} else {
+		// 4,5,6 or 7
+		sendBuffer(~0xC0);
+		if (readInput()) {
+			// 4 or 5
+			sendBuffer(~0x20);
+			if (readInput()) {
+				return 4;
+			} else {
+				return 5;
+			}
+		} else {
+			// 6 or 7
+			sendBuffer(~0x80);
+			if (readInput()) {
+				return 6;
+			} else {
+				return 7;
+			}
+		}
+	}
+}
+
+byte scanFirst() {
+	sendBuffer(0);
+	byte adr = scanLine();
+	if (adr == 0xFF) {
+		return 0xFF;
+	}
+	delay(2);
+	adr = scanLine();
+	if (adr == 0xFF) {
+		return 0xFF;
+	}
+	return (findLine() << 4) | adr;
 }
 
 void scanAll() {
@@ -88,23 +151,23 @@ void setup() {
 	Serial.begin(115200);
 
 	pinMode(K_OUT_CLOCK, OUTPUT);
-	pinMode(K_OUT_CLEAR, OUTPUT);
-	pinMode(K_OUT_DATA , OUTPUT);
+//	pinMode(K_OUT_CLEAR, OUTPUT);
+//	pinMode(K_OUT_DATA , OUTPUT);
  	pinMode(K_IN_A     , OUTPUT);
  	pinMode(K_IN_B     , OUTPUT);
  	pinMode(K_IN_C     , OUTPUT);
  	pinMode(K_IN_G     , OUTPUT);
- 	pinMode(K_IN_DATA  , INPUT_PULLUP);
+// 	pinMode(K_IN_DATA  , INPUT_PULLUP);
 
  	digitalWrite(K_IN_G, HIGH);
- 	digitalWrite(K_OUT_CLOCK, LOW);
- 	digitalWrite(K_OUT_CLEAR, HIGH);
+ 	digitalWrite(K_OUT_CLOCK, HIGH);
+// 	digitalWrite(K_OUT_CLEAR, HIGH);
 
  	registerInput(sizeof(inputs), inputs);
 
- 	Serial.print(mapping[2][5]);
- 	Serial.print(mapping[0][0]);
- 	Serial.print(mapping[7][7]);
+ 	Serial.print(mapping[2][2]);
+ 	Serial.print(mapping[0][7]);
+ 	Serial.print(mapping[7][0]);
  	Serial.println(" => A3^ ?");
 
  	Serial.println("Setup OK");
@@ -113,14 +176,18 @@ void setup() {
 //char *key = 0;
 byte key = 0xFF;
 void loop() {
+	unsigned long before = micros();
 	byte newKey = scanFirst();
+	unsigned long after = micros();
 	if (newKey != key) {
 		key = newKey;
 		if (key != 0xFF) {
 			Serial.print(key, HEX);
 			Serial.print('\t');
 			Serial.println(mapping[key >> 4][key & 7]);
-
+			Serial.println(after-before);
+		} else {
+			Serial.println("--");
 		}
 	}
 
